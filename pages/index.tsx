ea1b52141a59cb1965a7de452f625b8152a69d94
alienatedlabor <104693087+Alienatedlabor@users.noreply.github.com>
2023-04-18 16:43:38 -0400

    add image, style text

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,9 +2,12 @@ import Head from 'next/head';
 import { BsFillMoonStarsFill } from 'react-icons/bs';
 import { SiGithub, SiLinkedin } from 'react-icons/si';
 import { Figtree } from 'next/font/google';
+import Image from 'next/image';
+import avatar from '../public/ReadyPlayerMe-Avatar.png';
+
+//function for google font, allows preload of font
 const figtree = Figtree({ subsets: ['latin'] });
 
-console.log(figtree);
 export default function Home() {
   return (
     <div className={figtree.className}>
@@ -17,7 +20,9 @@ export default function Home() {
           <nav className="p-5 mb-12 flex justify-between">
             <h1 className="text-xl"></h1>
             <ul className="flex items-center gap-2">
-              <li>{/* <BsFillMoonStarsFill /> */}</li>
+              <li>
+                <BsFillMoonStarsFill />
+              </li>
               <li>
                 <a
                   className=" hover:shadow-md hover:opacity-90  bg-gradient-to-r from-cyan-900 to-cyan-600 text-white px-4 py-2 rounded-md "
@@ -28,21 +33,30 @@ export default function Home() {
               </li>
             </ul>
           </nav>
-          <div className="flex flex-col text-center">
-            <h2 className=" text-xl font-bold">Cole Heideman</h2>
-            <h3 className="text-lg">Frontend Web Developer</h3>
-            <p>
+          <div className="flex flex-col text-center p-10">
+            <h2 className=" text-5xl font-medium py-2 text-cyan-900">
+              Cole Heideman
+            </h2>
+            <h3 className="text-2xl py-2">Frontend Web Developer</h3>
+            <p className="text-md leading-8 text-gray-800">
               I'm passionate about creating dynamic and engaging user
-              experiences that are functional and intuitive, constantly striving
-              to learn and grow. When I'm not coding you'll find me improving my
-              German, playing guitar, or getting better at my current favorite
-              game.
+              experiences that are functional and intuitive, and I'm constantly
+              striving to learn and grow. When I'm not coding you'll find me
+              improving my German, playing guitar, or getting better at my
+              current favorite game.
             </p>
           </div>
 
-          <div className="flex justify-center text-lg gap-3 my-2">
-            <SiGithub />
-            <SiLinkedin />
+          <div className="bg-gradient-to-b from-cyan-900 to-cyan-600 rounded-full relative mx-auto w-80 h-80 overflow-hidden">
+            <Image
+              src={avatar}
+              alt="3d digital representation of smiling bald bearded man with glasses (me) "
+              fill
+            />
+          </div>
+          <div className="flex justify-center text-5xl gap-12 py-4 text-cyan-900">
+            <SiGithub className="cursor-pointer" />
+            <SiLinkedin className="cursor-pointer" />
           </div>
         </section>
       </main>
